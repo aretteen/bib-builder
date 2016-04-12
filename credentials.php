fa1ec7b45b27d411097a5301c0f570f94a4cefb3
aretteen <writedoc@gmail.com>
2016-04-12 13:37:38 -0400

    Added files via upload

---
+++ credentials.php
@@ -0,0 +1,18 @@
+<?php
+//
+// Provides config.php with the proper credentials to access the Zotero API.
+// 
+
+// API Keys and Read/Write Permissions can be configured from Zotero.org.  
+// Copy and paste the API Key into the appropriate place below.
+// Place the API key after the = sign and before the ending quotation mark.
+
+$apiR = "key="; // For display and search funcationality, you only need a key allowing Read access.
+
+$apiRW = "key="; // For functionality supporting community additions through BibBuilder, Read/Write access is needed.
+
+// Put the proper Group ID number below, and do not use quotations.  Replace "000000" with your Group ID.
+
+$groupID = 000000;
+
+?>
